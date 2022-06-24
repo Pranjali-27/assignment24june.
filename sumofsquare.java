43523968907a9f5f28c143e3d1ad6469bd2638d1
Pranjali-27 <106953217+Pranjali-27@users.noreply.github.com>
2022-06-24 00:57:42 -0700

    Add files via upload

---
+++ sumofsquare.java
@@ -0,0 +1,53 @@
+import java .util.*;
+class sumofsquare
+{
+public static void main(String[] args)
+{
+                     int num=1;
+                     int s= 0;
+
+System.out.println("\t\t\tNumber   Square");
+
+              while(num<=10)
+               {
+              int square=num*num;
+
+          System.out.println("\t\t\t"+num+"      "+square);
+
+           s =s+square;
+ 
+
+
+            num++;
+            }
+
+             System.out.println("\t\t\t\s\t"+s);
+                }
+
+
+       
+
+               }
+
+
+
+
+C:\Users\AT SYSTEMS\SkyDrive\Documents\24juneassignment>javac sumofsquare.java
+
+C:\Users\AT SYSTEMS\SkyDrive\Documents\24juneassignment>javac sumofsquare.java
+
+C:\Users\AT SYSTEMS\SkyDrive\Documents\24juneassignment>java sumofsquare.java
+                        Number   Square
+                        1      1
+                        2      4
+                        3      9
+                        4      16
+                        5      25
+                        6      36
+                        7      49
+                        8      64
+                        9      81
+                        10      100
+                                385
+
+C:\Users\AT SYSTEMS\SkyDrive\Documents\24juneassignment>
